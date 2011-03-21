4a3fd55e6a4888d55eeffc18ac5d268bedb2f984
shizhao <shizhao@gmail.com>
2011-03-22 00:42:28 +0800

    config

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// registering your application: https://foursquare.com/oauth/
+$clientId = ''; //Your CLIENT ID
+$clientSecret = ''; //CLIENT SECRET
+$redirectUri = ''; //CALLBACK URL, is http://path/index.php, path is your URL
+$latidude_code = ''; ///* google latitude user id... you can find it at http://www.google.com/latitude/apps/badge
+?>
